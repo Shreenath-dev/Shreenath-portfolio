06b6dbd19043a861eb7cb46568be100e1d19f28c
shreenathdev <shreenath.py@gmail.com>
2026-08-17 13:49:21 +0530

    Update Leadfeeder tracking script

--- app/layout.tsx
+++ app/layout.tsx
@@ -116,7 +116,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         </script>
         <script
           dangerouslySetInnerHTML={{
-            __html: `(function(ss,ex){ window.ldfdr=window.ldfdr||function(){(ldfdr._q=ldfdr._q||[]).push([].slice.call(arguments));}; (function(d,s){ fs=d.getElementsByTagName(s)[0]; function ce(src){ var cs=d.createElement(s); cs.src=src; cs.async=1; fs.parentNode.insertBefore(cs,fs); }; ce('https://sc.lfeeder.com/lftracker_v1_'+ss+(ex?'_'+ex:'')+'.js'); })(document,'script'); })('Xbp1oaE0WQk4EdVj');`,
+            __html: `(function(ss,ex){ window.ldfdr=window.ldfdr||function(){(ldfdr._q=ldfdr._q||[]).push([].slice.call(arguments));}; (function(d,s){ fs=d.getElementsByTagName(s)[0]; function ce(src){ var cs=d.createElement(s); cs.src=src; cs.async=1; fs.parentNode.insertBefore(cs,fs); }; ce('https://sc.lfeeder.com/lftracker_v1_'+ss+(ex?'_'+ex:'')+'.js'); })(document,'script'); })('ywVkO4XlVBW4Z6Bj');`,
           }}
         />
       </head>
